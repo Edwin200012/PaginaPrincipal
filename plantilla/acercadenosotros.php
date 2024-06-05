dc04706371570ce9d16e85465e624f354ffd1589
Edwin200012 <edwin_vazquez200012@hotmail.com>
2024-06-05 13:09:22 -0600

    Crear funcion para obtener los datos y mostrar descripcion, mision y vision

--- plantilla/acercadenosotros.php
+++ plantilla/acercadenosotros.php
@@ -1,6 +1,4 @@
-<!-- <link href="assets/css/style.css" rel="stylesheet"> -->
 <link href="../assets/css/style.css" rel="stylesheet">
- <!-- ======= About Us Section ======= -->
  <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
@@ -10,30 +8,44 @@
 
         <div class="row content">
           <div>
-            <p class="centered-text" style="margin-bottom: 2%;">
-              <!-- En Arsha, estamos comprometidos a ofrecer soluciones de software innovadoras y de alta calidad que impulsen el éxito de nuestros clientes. 
-              Desde nuestra fundación en 2010, hemos estado liderando el camino en el desarrollo de software, proporcionando servicios personalizados que satisfacen las necesidades únicas de cada cliente.<br>
-              Arsha fue fundada por un equipo de apasionados por la tecnología que compartían una visión común: simplificar la vida de las personas a través de soluciones digitales inteligentes. 
-              Lo que comenzó como un pequeño proyecto en un garaje se ha convertido en una empresa reconocida a nivel nacional por su excelencia en el desarrollo de software. -->
+            <p id="mostrarDescripcion" class="centered-text" style="margin-bottom: 2%;">
             </p>
           </div>
           <div class="col-lg-6">
             <h3>Misión</h3>
-            <p>
-              <!-- Nuestra misión en Arsha es aprovechar la tecnología para transformar la forma en que las empresas operan y se relacionan con sus clientes. 
-              Estamos dedicados a proporcionar soluciones de software que sean intuitivas, confiables y que impulsen el crecimiento empresarial.
-              En Arsha, estamos comprometidos a ofrecer soluciones de software innovadoras y de alta calidad que impulsen el éxito de nuestros clientes.
-              Desde nuestra fundación en 2010, hemos estado liderando el camino en el desarrollo de software, proporcionando servicios personalizados que satisfacen las necesidades únicas de cada cliente. -->
+            <p id="mostrarMision">
             </p>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <h3>Visión</h3>
-            <p>
-              <!-- En Arsha, nuestra visión es liderar la vanguardia de la innovación tecnológica, ofreciendo soluciones de software de vanguardia que transformen industrias y mejoren la vida de las personas en todo el mundo.
-              Nos esforzamos por ser reconocidos como el socio preferido para empresas que buscan impulsar su éxito a través de la tecnología, destacando por nuestra dedicación a la excelencia, la innovación constante y la colaboración con nuestros clientes para superar desafíos y alcanzar metas ambiciosas. -->
+            <p id="mostrarVision">
             </p>
           </div>
         </div>
       </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+      <script>
+        $(document).ready(function() {
+            mostrarDatosNosotrosPublicado();
+        })
+
+        function mostrarDatosNosotrosPublicado(){
+            jQuery.ajax({
+                url: 'controllers/acercadenosotrospublicado.php',
+                type: 'GET',
+                dataType: 'JSON',
+                success: function (response){
+                    let datos = response.registronosotrospublicado;
+                     // Iterar sobre los datos y mostrar informacion
+                    datos.forEach(item => {
+                        $('#mostrarDescripcion').append(item.descripcion);
+                        $('#mostrarMision').append(item.mision);
+                        $('#mostrarVision').append(item.vision);
+            });
+                }
+            }).fail(function() {
+                alert("Error");
+            });
+        }
+      </script>
     </section>
-    <!-- End About Us Section -->
