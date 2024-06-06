eafa0c38b5c652fc7c480c8b61bb50469b380c0c
Edwin200012 <edwin_vazquez200012@hotmail.com>
2024-06-06 15:52:01 -0600

    Crear secciones de equipo y servicios

---
+++ secciones/equipo.php
@@ -0,0 +1,84 @@
+<div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Equipo</h2>
+          <p>
+          En Arsha, contamos con un equipo altamente calificado y apasionado que está dedicado a ofrecer soluciones innovadoras y de alta calidad a nuestros clientes. 
+          Nuestro equipo diverso y multidisciplinario está compuesto por profesionales talentosos que aportan una amplia gama de habilidades y experiencias a cada proyecto que emprendemos. 
+          A continuación, te presentamos a algunos de los miembros destacados de nuestro equipo:
+          </p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Walter White</h4>
+                <span>Chief Executive Officer</span>
+                <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Sarah Jhonson</h4>
+                <span>Product Manager</span>
+                <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="300">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>William Anderson</h4>
+                <span>CTO</span>
+                <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-4.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Amanda Jepson</h4>
+                <span>Accountant</span>
+                <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
---
+++ secciones/servicios.php
@@ -0,0 +1,47 @@
+<div class="container" data-aos="fade-up">
+
+<div class="section-title">
+  <h2>Servicios</h2>
+  <p>
+  En Arsha, nos enorgullecemos de ofrecer una amplia gama de servicios diseñados para satisfacer las necesidades de nuestros clientes y ayudarlos a alcanzar sus objetivos. 
+  Nuestro equipo de expertos está comprometido a brindar soluciones personalizadas y de alta calidad en cada proyecto que emprendemos. 
+  A continuación, te ofrecemos una descripción general de los servicios que ofrecemos:
+  </p>
+</div>
+
+<div class="row">
+  <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+    <div class="icon-box">
+      <div class="icon"><i class="bx bxl-dribbble"></i></div>
+      <h4><a href="">Lorem Ipsum</a></h4>
+      <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+    </div>
+  </div>
+
+  <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+    <div class="icon-box">
+      <div class="icon"><i class="bx bx-file"></i></div>
+      <h4><a href="">Sed ut perspici</a></h4>
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+    </div>
+  </div>
+
+  <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+    <div class="icon-box">
+      <div class="icon"><i class="bx bx-tachometer"></i></div>
+      <h4><a href="">Magni Dolores</a></h4>
+      <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+    </div>
+  </div>
+
+  <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+    <div class="icon-box">
+      <div class="icon"><i class="bx bx-layer"></i></div>
+      <h4><a href="">Nemo Enim</a></h4>
+      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+    </div>
+  </div>
+
+</div>
+
+</div>
